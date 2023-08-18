eae754064da7bec0b6388fff555f0495f42b23ab
Victorio <victoriocaccioppoli@gmail.com>
2023-08-17 22:39:51 -0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,105 @@
 import { Inputs } from "./inputs";
+import { useState } from "react";
 
 function App() {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleItemClick = (event) => {
+    const selectedElement = event.target;
+
+    if (selectedElement.tagName === "SPAN") {
+      selectedElement.classList.toggle("selected");
+    }
+  };
+
   return (
     <>
       <div className="division">
-          <h1>Monstruo</h1>
+        <h1>Monstruo</h1>
         <ul>
-          <li>Drácula
-            <Inputs/>
+          <li>
+            <span onClick={handleItemClick}>Dracula</span>
+            <Inputs />
           </li>
-          <li>Frankenstein  <Inputs/></li>
-          <li>El hombre lobo  <Inputs/></li>
-          <li>Fantasma  <Inputs/></li>
-          <li>La momia  <Inputs/></li>
-          <li>Dr.Jekyll y Mr Hyde  <Inputs/></li>
+          <li>
+            <span onClick={handleItemClick}>Frankenstein</span>
+            <Inputs />
+          </li>
+          <li>
+            <span onClick={handleItemClick}>Hombre Lobo</span>
+            <Inputs />
+          </li>
+          <li>
+            <span onClick={handleItemClick}>Fantasma</span>
+            <Inputs />
+          </li>
+          <li>
+            <span onClick={handleItemClick}>Momia</span>
+            <Inputs />
+          </li>
+          <li>
+            <span onClick={handleItemClick}>Dr Jekyll Mr Hyde</span>
+            <Inputs />
+          </li>
         </ul>
       </div>
       <div className="division">
-          <h1>Víctima </h1>
+        <h1>Víctima </h1>
         <ul>
-          <li>Conde  <Inputs/></li>
-          <li>Condesa  <Inputs/></li>
-          <li>Ama de llaves  <Inputs/></li>
-          <li>Mayordomo  <Inputs/> </li>
-          <li>Doncella <Inputs/></li>
-          <li>Jardinero <Inputs/> </li>
+          <li>
+            <span onClick={handleItemClick}>Conde</span>
+            <Inputs />
+          </li>
+          <li>
+            <span onClick={handleItemClick}>Condesa</span>
+            <Inputs />
+          </li>
+          <li>
+            <span onClick={handleItemClick}>Ama de llaves</span>
+            <Inputs />
+          </li>
+          <li>
+            <span onClick={handleItemClick}>Mayordomo</span>
+            <Inputs />{" "}
+          </li>
+          <li>
+            <span onClick={handleItemClick}>Doncella</span> <Inputs />
+          </li>
+          <li>
+            <span onClick={handleItemClick}>Jardinero</span> <Inputs />{" "}
+          </li>
         </ul>
       </div>
       <div className="division">
-          <h1>Lugar </h1>
+        <h1>Lugar </h1>
         <ul>
-          <li>Vestibulo  <Inputs/></li>
-          <li>Alcoba  <Inputs/></li>
-          <li>Biblioteca  <Inputs/></li>
-          <li>Salon  <Inputs/></li>
-          <li>Laboratorio  <Inputs/></li>
-          <li>Bodega  <Inputs/></li>
-          <li>Panteon  <Inputs/></li>
-          <li>Cocheras  <Inputs/></li>
+          <li>
+            <span onClick={handleItemClick}>Vestibulo</span> <Inputs />
+          </li>
+          <li>
+            <span onClick={handleItemClick}>Alcoba</span> <Inputs />
+          </li>
+          <li>
+            <span onClick={handleItemClick}>
+              Biblioteca
+            </span>{" "}
+            <Inputs />
+          </li>
+          <li>
+            <span onClick={handleItemClick}>Salon</span> <Inputs />
+          </li>
+          <li>
+            <span onClick={handleItemClick}>Laboratorio</span> <Inputs />
+          </li>
+          <li>
+            <span onClick={handleItemClick}>Bodega</span> <Inputs />
+          </li>
+          <li>
+            <span onClick={handleItemClick}>Panteon</span> <Inputs />
+          </li>
+          <li>
+            <span onClick={handleItemClick}>Cocheras</span> <Inputs />
+          </li>
         </ul>
       </div>
     </>
